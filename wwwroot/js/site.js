bf01717ed2a0f0a8386fc25825b67152cf4f331b
Starting Web <startingwebtec@gmail.com>
2025-10-24 14:55:18 -0300

    Push Inicial

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
---
+++ wwwroot/js/tailwind.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+    content: [
+        "./Pages/**/*.{cshtml,razor,html}",
+        "./Views/**/*.{cshtml,razor,html}"
+    ],
+    theme: { extend: {} },
+    plugins: [],
+};
